e1c5d73a4671c1c59c926d19ab83ba82f6016e9d
Mohan Rajbhar <m.rajbhar1235@gmail.com>
2024-04-01 23:24:54 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -169,64 +169,6 @@ function App() {
         </div>
       </nav>
 
-      {/* <header
-        className={`${
-          scrolling ? "border-b border-gray-900" : ""
-        }  fixed left-0 right-0 top-0 z-20`}
-        id="home"
-      >
-        <div
-          className={`container m-auto px-4 py-6 max-w-4xl ${
-            theme === "dark" ? "bg-black" : "bg-white"
-          }`}
-        >
-          <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
-            <div>
-              <h1 className="font-bold text-2xl">Mohan Portfolio</h1>
-            </div>
-            <div>
-              <ul className="flex gap-4">
-                <li>
-                  <a
-                    href="#projects"
-                    className="text-gray-400 hover:text-white cursor-pointer"
-                  >
-                    Projects
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#technologies"
-                    className="text-gray-400 hover:text-white cursor-pointer"
-                  >
-                    Technologies
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#aboutme"
-                    className="text-gray-400 hover:text-white cursor-pointer"
-                  >
-                    About me
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#hireme"
-                    className="text-gray-400 hover:text-white cursor-pointer"
-                  >
-                    Contact
-                  </a>
-                </li>
-                <button onClick={toggleTheme}>
-                  {theme === "light" ? <FaMoon /> : <FaSun />}
-                </button>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </header> */}
-
       <main className="relative mt-28">
         {/* Intro/Banner section */}
         <section>
